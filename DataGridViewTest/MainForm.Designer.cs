8b4021e0c6540aa83476cbffa0443bc40f61318c
timofeev <timofeev.simeon@hotmail.com>
2018-07-30 01:55:46 +0300

    Add project files.

---
+++ DataGridViewTest/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DataGridViewTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this._toolStrip = new System.Windows.Forms.ToolStrip();
+            this._dgv = new System.Windows.Forms.DataGridView();
+            this._tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _tableLayoutPanel
+            // 
+            this._tableLayoutPanel.ColumnCount = 1;
+            this._tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this._tableLayoutPanel.Controls.Add(this._toolStrip, 0, 0);
+            this._tableLayoutPanel.Controls.Add(this._dgv, 0, 1);
+            this._tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this._tableLayoutPanel.Name = "_tableLayoutPanel";
+            this._tableLayoutPanel.RowCount = 2;
+            this._tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this._tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this._tableLayoutPanel.Size = new System.Drawing.Size(800, 450);
+            this._tableLayoutPanel.TabIndex = 0;
+            // 
+            // _toolStrip
+            // 
+            this._toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this._toolStrip.Location = new System.Drawing.Point(0, 0);
+            this._toolStrip.Name = "_toolStrip";
+            this._toolStrip.Size = new System.Drawing.Size(800, 25);
+            this._toolStrip.TabIndex = 0;
+            this._toolStrip.Text = "toolStrip1";
+            // 
+            // _dgv
+            // 
+            this._dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._dgv.Location = new System.Drawing.Point(3, 28);
+            this._dgv.Name = "_dgv";
+            this._dgv.Size = new System.Drawing.Size(794, 419);
+            this._dgv.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._tableLayoutPanel);
+            this.Name = "MainForm";
+            this.Text = "DataGridView test application";
+            this._tableLayoutPanel.ResumeLayout(false);
+            this._tableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel _tableLayoutPanel;
+        private System.Windows.Forms.ToolStrip _toolStrip;
+        private System.Windows.Forms.DataGridView _dgv;
+    }
+}
+
---
+++ DataGridViewTest/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataGridViewTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitToolStrip();
+            InitDataGridView();
+        }
+
+        private void InitToolStrip()
+        {
+            ToolStripButton fillTSB = new ToolStripButton("Заполнить");
+            fillTSB.Click += (s, e) => FillDGV();
+            _toolStrip.Items.Add(fillTSB);
+        }
+        
+        private void InitDataGridView()
+        {
+            DataGridViewCellStyle style = new DataGridViewCellStyle
+            {
+                WrapMode = DataGridViewTriState.False
+            };
+
+            _dgv.DefaultCellStyle = style;
+
+            _dgv.Columns.Add(new DataGridViewTextBoxColumn { Name = "Column_1", HeaderText = "Столбец 1" });
+            _dgv.Columns.Add(new DataGridViewTextBoxColumn { Name = "Column_2", HeaderText = "Столбец 2" });
+            _dgv.Columns.Add(new DataGridViewTextBoxColumn { Name = "Column_3", HeaderText = "Столбец 3" });
+            _dgv.Columns.Add(new DataGridViewTextBoxColumn { Name = "Column_4", HeaderText = "Столбец 4" });
+            _dgv.Columns.Add(new DataGridViewTextBoxColumn { Name = "Column_5", HeaderText = "Столбец 5" });
+        }
+
+        //------------------------------------------------------------------------
+
+        Random _randoizer;
+        Random Randomizer => _randoizer ?? (_randoizer = new Random());
+
+        private void FillDGV()
+        {
+            try
+            {
+                int rowsCount = Randomizer.Next(10, 50);
+
+                _dgv.Rows.Clear();
+
+                for (int i = 0; i < rowsCount; i++)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    for (int j = 0; j < _dgv.ColumnCount; j++)
+                    {
+                        if (j == 0)
+                            row.Cells.Add(new DataGridViewTextBoxCell() { Value = $"[{i};{j}]" });
+                        if (j == 1)
+                            row.Cells.Add(new DataGridViewTextBoxCell() { Value = $"[{i};{j}]" });
+                        if (j == 2)
+                            row.Cells.Add(new DataGridViewTextBoxCell() { Value = $"[{i};{j}]" });
+                        if (j == 3)
+                            row.Cells.Add(new DataGridViewTextBoxCell() { Value = $"[{i};{j}]" });
+                        if (j == 4)
+                            row.Cells.Add(new DataGridViewTextBoxCell() { Value = $"[{i};{j}]" });
+                    }
+
+                    _dgv.Rows.Add(row);
+                }
+            }
+            catch (Exception error)
+            {
+                System.Diagnostics.Debug.WriteLine(error.Message);
+            }
+        }
+    }
+}
